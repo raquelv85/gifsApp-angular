7976eb6d9e5c0f24ffdd95e1ad669a58af2b0920
Raquel Villaverde Palma <raquelv85@gmail.com>
2022-03-15 09:32:38 +0100

    create Service and control searches

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private _history: string[] = [];
+
+  get history() {
+    return [...this._history];
+  }
+
+  searchGifs(query: string) {
+    query = query.trim().toLocaleLowerCase();
+    if (!this._history.includes(query)) {
+      this._history.unshift(query);
+      this._history = this._history.splice(0, 10);
+    }
+    
+    console.log(this._history);
+  }
+}
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get history() {
+    return this.gifsService.history;
   }
+
+  constructor(private gifsService: GifsService) {}
 
 }
